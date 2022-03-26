6534eace7a3267b301ac6960c6fa98dfcaf834ba
asshames <assamialomarshames@gmail.com>
2022-03-26 19:12:06 +0600

    adding react and ajax and a blog container

---
+++ bloger/src/containers/Blog/Blog.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Blog extends Component{
+    render() {
+        return (
+            <div>
+                <h1>Here is the list of Blog</h1>
+            </div>
+        );
+    };
+};
+
+export default Blog;
