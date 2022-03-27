cf3ed876191bb7409eda270cff11bbb03f437981
asshames <assamialomarshames@gmail.com>
2022-03-27 14:11:30 +0600

    adding axios pakaged

--- bloger/src/containers/Blog/Blog.js
+++ bloger/src/containers/Blog/Blog.js
@@ -1,10 +1,24 @@
 import React, { Component } from "react";
+import axios from "axios";
+
 import classes from "./Blog.module.css";
 import Post from "../../components/Post/Post";
 import FullPost from "../../components/FullPost/FullPost";
 import FavoritePost from "../../components/FavoritePost/FavoritePost";
 
 class Blog extends Component{
+    state = {
+        posts: [],
+        postSelectedId: null
+    }
+
+    componentDidMount() {
+        axios.get("https://jsonplaceholder.typicode.com/posts")
+            .then(response => {
+                this.setState({ posts: response.data });
+            });
+    }
+
     render() {
         return (
             <div className={classes.Blog}>
