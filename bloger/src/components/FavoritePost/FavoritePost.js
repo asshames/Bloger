35fd5358dc3804be3a7c03af0d186dc442069a05
asshames <assamialomarshames@gmail.com>
2022-03-31 16:51:57 +0600

    implement code for Favorite Post

---
+++ bloger/src/components/FavoritePost/FavoritePost.js
@@ -0,0 +1,46 @@
+import React from "react";
+import classes from "./FavoritePost.module.css";
+import Button from "../Button/Button";
+
+class FavoritePost extends Component {
+    state = {
+        favoritePost: null
+    }
+
+    componentDidUpdate() {
+        if (this.props.id) {
+            if (!this.state.favoritePost || (this.state.favoritePost && this.state.favoritePost.id !== this.props.id)) {
+                axios.get('/posts/' + this.props.id)
+                    .then(response => {
+                        this.setState({ favoritePost: response.data });
+                    });
+            }
+        }
+    }
+
+    postDeleteHandler = () => {
+        axios.delete('/posts/' + this.props.id)
+            .then(response => {
+                console.log(response);
+            })
+    }
+
+    render() {
+        let post = <p style={{ textAlign: 'center' }}>Please select for adding Favorite Post!</p>;
+        if (this.props.id) {
+            post = <p style={{ textAlign: 'center' }}>Loading........!</p>;
+        }
+        if (this.state.favoritePost) {
+            post = (
+                <div className={classes.FavoritePost}>
+                    <h1>{this.state.favoritePost.title}</h1>
+                    <p>{this.state.favoritePost.body}</p>
+                    <div className="Edit">
+                        <Button className="Delete" onClick={this.postDeleteHandler}>Delete</Button>
+                    </div>
+                </div>
+            );
+        }
+    }
+}
+export default FavoritePost;
