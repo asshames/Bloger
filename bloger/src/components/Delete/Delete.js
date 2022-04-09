6cf4368047cda26e378095a7b88b61b2b4a64a69
asshames <assamialomarshames@gmail.com>
2022-04-09 23:02:45 +0600

    add some css and rearrange the delete component code

--- bloger/src/components/Delete/Delete.js
+++ bloger/src/components/Delete/Delete.js
@@ -1,10 +1,6 @@
 import React from "react";
 import classes from "./Delete.module.css";
 
-const Delete = (props) => (
-    <button
-        className={classes.Delete}
-        onClick={props.clicked}>{props.children}</button>
-);
+const Delete = (props) => <button className={classes.Delete} onClick={props.clicked}>{props.children}</button>;
 
 export default Delete;
